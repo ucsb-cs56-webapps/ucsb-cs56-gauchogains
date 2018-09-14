bb15684a09c2869e4ee8c6c0ce44f30654b62021
Maga Kim <maga@umail.ucsb.edu>
2018-09-13 19:55:15 -0700

    Interfaced frontend to MongoDB

--- src/main/java/edu/ucsb/cs56/gaucho_gains/Project.java
+++ src/main/java/edu/ucsb/cs56/gaucho_gains/Project.java
@@ -1,5 +1,7 @@
 package edu.ucsb.cs56.gaucho_gains;
 
+import edu.ucsb.cs56.gaucho_gains.GainsDatabase;
+
 import static spark.Spark.port;
 
 import org.apache.log4j.Level;
@@ -15,58 +17,53 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 
 public class Project {
-    public static final String CLASSNAME="Project";
-    public static final Logger log = Logger.getLogger(CLASSNAME);
-    
-    public static void main(String[] args) {
-	log.setLevel(Level.INFO);
+	public static final String CLASSNAME="Project";
+	public static final Logger log = Logger.getLogger(CLASSNAME);
 
-	log.trace("Trace Message!");
-	log.debug("Debug Message!");
-	log.info("Info Message!");
-	log.warn("Warn Message!");
-	log.error("Error Message!");
-	log.fatal("Fatal Message!");
-	
-        port(getHerokuAssignedPort());
+	public static void main(String[] args) {
+		log.setLevel(Level.INFO);
 
-	/* Dummy Database */
-	HashMap<String, Object> model = new HashMap<String, Object>();
+		log.trace("Trace Message!");
+		log.debug("Debug Message!");
+		log.info("Info Message!");
+		log.warn("Warn Message!");
+		log.error("Error Message!");
+		log.fatal("Fatal Message!");
 
-	/* Login Page */
-	get("/", (rq, rs) ->  new ModelAndView(model, "login.mustache"), new MustacheTemplateEngine());
+		GainsDatabase db = new GainsDatabase();
+		port(getHerokuAssignedPort());
 
-	/* Profile Page */
-	post("/profile", (rq, rs) -> {
-		String user = rq.queryParams("email");
-		String pass = rq.queryParams("pass");
-		UserManager um = new UserManager();
-		model.put("validUser2", um.addUser(user,pass));
-		model.put("user", user);
+		/* Dummy Database */
+		HashMap<String, Object> model = new HashMap<String, Object>();
 
-		return new ModelAndView(model, "profile.mustache");
-	    }, new MustacheTemplateEngine());
+		/* Login Page */
+		get("/", (rq, rs) ->  new ModelAndView(model, "login.mustache"), new MustacheTemplateEngine());
 
-	/* Next Page After Login */
-	post("/community", (rq, rs) -> {
-		log.info("post /community");
-		
-		String user = rq.queryParams("email");
-		String pass = rq.queryParams("pass");
-		UserManager um = new UserManager();
-		model.put("validUser", um.checkUser(user,pass));
-		model.put("user",user);
-		
-		return new ModelAndView(model, "community.mustache");
-	    }, new MustacheTemplateEngine());
+		/* Profile Page */
+		post("/profile", (rq, rs) -> {
+			if (!rq.queryParams("password").equals(rq.queryParams("passwordConfirm"))) {
+				model.put("errMess", "Passwords do not match");
+				return (new ModelAndView(model, "signuperror.mustache"));
+			}
+			String check = db.signUp(rq);
+                        model.put("errMess", check);
+                        return check.equals("Sign Up Success") ? (new ModelAndView(model, "profile.mustache")):(new ModelAndView(model, "signuperror.mustache"));
+		}, new MustacheTemplateEngine());
 
-	get("/signup", (rq, rs) -> new ModelAndView(model, "signup.mustache"), new MustacheTemplateEngine());
-    }
+		/* Next Page After Login */
+		post("/community", (rq, rs) -> {
+			String check = db.logIn(rq);
+			model.put("errMess", check);
+			return check.equals("Login Success") ? (new ModelAndView(model, "community.mustache")):(new ModelAndView(model, "signuperror.mustache"));
+		}, new MustacheTemplateEngine());
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null)
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }	
+		get("/signup", (rq, rs) -> new ModelAndView(model, "signup.mustache"), new MustacheTemplateEngine());
+	}
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null)
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	}	
 }
